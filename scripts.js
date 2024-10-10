7e679536b61c0d3842db5c5d34e86c051d083697
Abby Skillestad <askillestad@zagmail.gonzaga.edu>
2024-10-09 19:26:33 -0700

    added newArtworks and function layout

---
+++ scripts.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let viewedCount = 0;
+let removeMode = false;
+
+const newArtworks = [
+    { title: "The Scream", artist: 'Edvard Munch', img: 'https://via.placeholder.com/200' },
+    { title: 'Girl with a Pearl Earring', artist: 'Johannes Vermeer', img: 'https://via.placeholder.com/200' },
+    { title: 'The Birth of Venus', artist: 'Sandro Botticelli', img: 'https://via.placeholder.com/200' },
+];
+
+// function to update viewed counter
+
+
+// function to handle art panel clicks
+
+
+// function to reset gallery
+
+
+// function to add new artwork
+
+
+// function to toggle remove mode/button
+
+
+// event listeners for DOM
